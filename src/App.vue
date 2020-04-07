e8bb3869a645ed45a282521a4308dd1987021ae8
harumakiyukko <y.toyama01@gmail.com>
2020-04-07 10:41:36 +0900

    githubのリンク追加

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
       <div class="logotext">
         <h2>#stayhome のために、癒しになるようなSNS投稿を集めています。</h2>
         <p>偏りなく集めていきたいと思いますが、気づいたことがあれば<a href="https://twitter.com/harumakiyukko">twitter</a>までご連絡下さい。<br>
-          共同編集はgithubかgoogleスプレッドシートでやりたいと思いますので、一緒に取り組んでくださる方からのご連絡もお待ちしております。</p>
+          共同編集は<a href="https://github.com/harumakiyukko/stayhome">github</a>とgoogleスプレッドシート（準備中）でやりたいと思いますので、一緒に取り組んでくださる方からのご連絡もお待ちしております。</p>
         <p>
           thank you all !<br>
           art work : <a href="https://twitter.com/Hashi_ko845">@Hashi_ko845</a><br>
